e5c7bd84956403662246bbed3e77c6f0e71dc37c
Vicky <vy4693@gmail.com>
2022-10-23 23:20:07 +0530

    landing page changes

---
+++ src/views/home/Features.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import { Box } from "@mui/material";
+import './features.css'
+import lappy from "../../assets/images/landinglappy.png"
+import mobile from "../../assets/images/landingmobile.png"
+
+const FeaturesBottom = () => {
+    return (<section >
+        <Box mt={10} className={'featuresContainer'}>
+            <section className={'leftFeaturesContainer'} >
+                <div>
+                    <img className="landingLappy" src={lappy} />
+                </div>
+                <div>
+                    <img className="landingMobile" src={mobile} />
+                </div>
+            </section>
+            <section style={{ margin: 0 }} className={'rightFeaturesContainer'}>
+                <h1 className={'headerFeat'}>Book and Bid with a single click</h1>
+                <h3 style={{ color: '#a6a6a6', fontWeight: '500', lineHeight: '40px' }}>Streamline your bookings with Book Now and Bid funcitonality. No more phone calls or emails. With a single click you can book or bid on loads all within freightdok.</h3>
+            </section>
+        </Box>
+    </section>)
+}
+
+
+
+const FeaturesTop = () => {
+    return (<section >
+        <Box mt={15} className={'featuresContainer'}>
+            <section className="leftCommonCSS" >
+                <h1 className={'headerFeat'}>Finding freight, made easy</h1>
+                <h3 style={{ paddingLeft: '2.5rem', color: '#a6a6a6', fontWeight: '500', width: '75%' }}>Today, truckers spend hours of their day sourcing freight. We've made it our goal to change that by helping truckers find loads without having to search through hundreds of load boards.
+                </h3>
+            </section>
+            <section className="rightCommonCSS" >
+                <h1 style={{ paddingLeft: '0.5rem' }}>Automate your sourcing </h1>
+                <h3 style={{ paddingLeft: '0.5rem', color: '#a6a6a6', fontWeight: '500' }}>Interact with hundreds of loads sourced from various freight brokers all within freightdok desktop and mobile apps. </h3>
+
+                <h1 style={{ paddingLeft: '0.5rem' }}>Schedule Optimization </h1>
+                <h3 style={{ paddingLeft: '0.5rem', color: '#a6a6a6', fontWeight: '500' }}>Build a schedule that keeps your wheels turning. Increase utlization while decreasing deadhead. </h3>
+
+                <h1 style={{ paddingLeft: '0.5rem' }}>Real Loads </h1>
+                <h3 style={{ paddingLeft: '0.5rem', color: '#a6a6a6', fontWeight: '500' }}>No more fake, booked, or duplicat loads. freightdok automatically fetches loads that are open to book and bid in real time.
+
+                </h3>
+
+            </section>
+        </Box>
+    </section>)
+}
+
+
+
+
+
+const FeaturesFinal = () => {
+
+    return (<section >
+        <Box mt={15} className={'featuresContainer'}>
+            <section className="leftCommonCSS"  >
+                <h1 className={'headerFeat'}>Automate back office tasks </h1>
+                <h3 style={{ paddingLeft: '2.5rem', color: '#a6a6a6', fontWeight: '500', width: '70%' }}>We know you're busy. That's why we created freightdok to help you save time and money with automated back office tasks.</h3>
+            </section>
+            <section className="rightCommonCSS" >
+                <h1 style={{ paddingLeft: '0.5rem' }}>Upload Documents </h1>
+                <h3 style={{ paddingLeft: '0.5rem', color: '#a6a6a6', fontWeight: '500' }}>Once load is delivered upload poof of delivery right from your phone. </h3>
+
+                <h1 style={{ paddingLeft: '0.5rem' }}>One Click Invoices
+
+                </h1>
+                <h3 style={{ paddingLeft: '0.5rem', color: '#a6a6a6', fontWeight: '500' }}>You're too busy to send out invoices manually. With one click you can create invocies. </h3>
+
+                <h1 style={{ paddingLeft: '0.5rem' }}>Capture Data </h1>
+                <h3 style={{ paddingLeft: '0.5rem', color: '#a6a6a6', fontWeight: '500' }}>
+                    No more manually writing down facility hours, emailing in/out times, or handling check calls. We will capture your data automatically so you can have it at your fingertips.
+                </h3>
+
+            </section>
+        </Box>
+    </section>)
+}
+
+
+const Footer = () => {
+    return (<footer className="footerMain">
+        <p style={{ marginLeft: '4rem' }}> Copyright &#169; 2022.  freightdok</p>
+        <div className="foot" style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <p>Indianapolis, Indiana</p>
+            <p>Terms of Use
+            </p>
+            <p>Privacy Policy
+            </p>
+        </div>
+    </footer>
+    )
+}
+
+
+const Features = () => {
+    return (<div className="featuresMainContainer">
+        <FeaturesTop />
+        <FeaturesBottom />
+        <FeaturesFinal />
+        <Footer />
+    </div>)
+}
+
+
+
+export default Features
--- src/views/home/Homepage.js
+++ src/views/home/Homepage.js
@@ -3,40 +3,49 @@ import { Box, Button, Stack, Link, Typography } from "@mui/material";
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import './styles.css'
 import { PRIMARY_BLUE } from "../../utils/utils";
+import Features from "./Features"
+
+const LandingPageHead = () => (<Box sx={{ position: 'relative' }} className={'landingPageContainer'}>
+    <Box component='main' p={8}>
+        <Stack direction='row' justifyContent='space-between' alignItems={'center'}>
+            <Typography sx={{ color: PRIMARY_BLUE, fontSize: 32, fontWeight: 700 }}>freightdok.</Typography>
+            <Link href="https://app.freightdok.io/login" underline="none" sx={{ color: "black", fontSize: 32, fontWeight: 700 }}>
+                {'Login'}
+            </Link>
+        </Stack>
+        <Box className={'landingPageMidSection'}>
+            <Stack sx={{ maxWidth: 600, mr: 5 }}>
+                <Typography className='sourceBookService' sx={{ color: PRIMARY_BLUE, fontSize: 56, fontWeight: 600 }}>Source.Book.Service</Typography>
+                <Typography className='yourLoadsWith' sx={{ color: '#000000', fontSize: 56, fontWeight: 600 }}>Your Loads with <Typography component={'span'} sx={{ color: PRIMARY_BLUE, fontSize: 56, fontWeight: 600 }}>{'Data'}</Typography></Typography>
+                <Typography sx={{ color: "#7d7d7d", fontSize: 22, fontWeight: 500, whiteSpace: 'pre' }} className='content'>
+                    <span>
+                        {'Source your loads, streamline your bookings, and automate\ncheck calls all within '}
+                    </span>
+                    <Typography component={'span'} sx={{ color: PRIMARY_BLUE, fontSize: 22, fontWeight: 600 }}>{'freightdok. '}</Typography>
+                    {'The new way of booking loads\nbacked by data.'}
+                </Typography>
+            </Stack>
+            <Stack mt={3}>
+                <Button variant={'contained'} className='contactUsButton'
+                    sx={{ py: '12px', width: 250, fontSize: 22, display: 'flex' }}
+                    endIcon={<ArrowForwardIcon />}
+                >
+                    Contact Us
+                </Button>
+            </Stack>
+        </Box>
+    </Box>
+    <div className='landingPageOval' />
+</Box>)
+
 
 const HomePage = () => {
-    return <Box sx={{ position: 'relative' }} className={'landingPageContainer'}>
-        <Box component='main' p={8}>
-            <Stack direction='row' justifyContent='space-between' alignItems={'center'}>
-                <Typography sx={{ color: PRIMARY_BLUE, fontSize: 32, fontWeight: 700 }}>freightdok.</Typography>
-                <Link href="https://app.freightdok.io/login" underline="none" sx={{ color:"black", fontSize: 22, fontWeight:700}}>
-                  {'Login'}
-                </Link>
-            </Stack>
-            <Box className={'landingPageMidSection'}>
-                <Stack sx={{ maxWidth: 600, mr: 5 }}>
-                    <Typography className='sourceBookService' sx={{ color: PRIMARY_BLUE, fontSize: 56, fontWeight: 600 }}>Source.Book.Service</Typography>
-                    <Typography className='yourLoadsWith' sx={{ color: '#000000', fontSize: 56, fontWeight: 600 }}>Your Loads with <Typography component={'span'} sx={{ color: PRIMARY_BLUE, fontSize: 56, fontWeight: 600 }}>{'Data'}</Typography></Typography>
-                    <Typography sx={{ color: "#7d7d7d", fontSize: 22, fontWeight: 500, whiteSpace: 'pre' }} className='content'>
-                        <span>
-                            {'Source your loads, streamline your bookings, and automate\ncheck calls all within '}
-                        </span>
-                        <Typography component={'span'} sx={{ color: PRIMARY_BLUE, fontSize: 22, fontWeight: 600 }}>{'freightdok. '}</Typography>
-                        {'The new way of booking loads\nbacked by data.'}
-                    </Typography>
-                </Stack>
-                <Stack mt={3}>
-                    <Button variant={'contained'} className='contactUsButton'
-                        sx={{ py: '12px', width: 250, fontSize: 22, display: 'flex'}}
-                        endIcon={<ArrowForwardIcon />}
-                    >
-                        Contact Us
-                    </Button>
-                </Stack>
-            </Box>
-        </Box>
-        <div className='landingPageOval' />
-    </Box>
+    return (<>
+
+        <LandingPageHead />
+        <Features />
+
+    </>)
 }
 
 export default HomePage
