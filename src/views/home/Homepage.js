2bee092227cf0ab04d7c8495865961e8b4f6c762
Vicky Yadav <vy4693@gmail.com>
2023-09-18 18:46:16 +0000

    updated contact to register and added partners sec

--- src/views/home/Homepage.js
+++ src/views/home/Homepage.js
@@ -4,6 +4,15 @@ import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import './styles.css'
 import { PRIMARY_BLUE } from "../../utils/utils";
 import Features from "./Features"
+import cw from "./../../assets/images/cw.png"
+import capstone from "./../../assets/images/capstone.png"
+import loadsmart from "./../../assets/images/ld.jpg"
+import ch from "./../../assets/images/ch.png"
+import everest from "./../../assets/images/everest.png"
+import slog from "./../../assets/images/sgog.png"
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const LandingPageHead = () => (<Box sx={{ position: 'relative' }} className={'landingPageContainer'}>
     <Box component='main' p={8}>
@@ -29,8 +38,11 @@ const LandingPageHead = () => (<Box sx={{ position: 'relative' }} className={'la
                 <Button variant={'contained'} className='contactUsButton'
                     sx={{ py: '12px', width: 250, fontSize: 22, display: 'flex' }}
                     endIcon={<ArrowForwardIcon />}
+                    onClick={() => {
+                        window.location.href = "https://app.freightdok.io/federalSignup"
+                    }}
                 >
-                    Contact Us
+                    Register
                 </Button>
             </Stack>
         </Box>
@@ -43,9 +55,80 @@ const HomePage = () => {
     return (<>
 
         <LandingPageHead />
+        <div style={{ background: "red", marginTop: "120px" }}>
+            <OurPartnersSection />
+        </div>
         <Features />
 
     </>)
 }
+
+
+
+const OurPartnersSection = () => {
+
+    const sliderSettings = {
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4,
+                },
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+        ],
+    };
+
+
+
+
+    return (<> <div className="container">
+      
+      <div style={{width:"100%"}}>
+      <h1  style={{color:"#0091ff",textAlign:"center",paddingBottom:"40px"}}>Our Partners</h1>
+       <div style={{marginLeft:"120px"}}>
+       <Slider {...sliderSettings} >
+            <div >
+                <img src={cw} alt="logo" />
+            </div>
+            <div >
+                <img src={capstone} alt="logo" />
+            </div>
+            <div >
+                <img src={ch} alt="logo" />
+            </div>
+            <div >
+                <img src={loadsmart} alt="logo" />
+            </div>
+            <div >
+                <img style={{height:"45px",objectFit:"contain"}}  src={everest} alt="logo" />
+            </div>
+            <div >
+                <img src={slog} alt="logo" />
+            </div>
+
+        </Slider>
+
+
+
+       </div>
+        </div>
+
+    </div></>)
+}
+
+
 
 export default HomePage
