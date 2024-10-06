91ccc1e7c6df493ec10d587793ff5443e248bd6f
jugalTiwari <times.jugal@gmail.com>
2024-10-06 14:15:29 +0530

    Fixed spacings and css in about us page

--- src/views/about_us/AboutUs.jsx
+++ src/views/about_us/AboutUs.jsx
@@ -2,7 +2,7 @@ import { Box, Container, Grid, Typography } from '@mui/material'
 import React, { useContext, useEffect } from 'react'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
-import { BG_COLOR, FONT_COLOR_SECONDARY } from '../../utils/utils'
+import { BG_COLOR, FONT_COLOR_PRIMARY, FONT_COLOR_SECONDARY } from '../../utils/utils'
 import { Context } from '../../providers/context'
 
 const AboutUs = () => {
@@ -15,29 +15,29 @@ const AboutUs = () => {
         <Box sx={{ height: '100dvh' }}>
             <Header />
             <Box>
-                <Container sx={{ p: 8 }}>
-                    <Typography color={'#000'} fontSize='2rem' align='center'>
+                <Container sx={{ py: 4 }}>
+                    <Typography color={FONT_COLOR_PRIMARY} fontSize='2.3rem' align='center'>
                         About Us
                     </Typography>
                 </Container>
                 <Box sx={{ bgcolor: BG_COLOR }}>
-                    <Container maxWidth='lg'>
-                        <Grid container flexWrap='wrap' sx={{ p: 8 }} alignItems='center'>
-                            <Grid item sm={6} xs={12}>
-                                <Typography color='#000' fontSize='3rem' align='center'>
+                    <Container maxWidth='lg' gap={{ sm: 5 }}>
+                        <Grid container flexWrap='wrap' sx={{ p: { xs: 4, sm: 8 } }} alignItems='center'>
+                            <Grid item sm={4} xs={12}>
+                                <Typography color='#000' fontSize='3rem' align='center' fontWeight={600}>
                                     Our Mission
                                 </Typography>
                             </Grid>
-                            <Grid item sm={6} xs={12}>
-                                <Typography color={FONT_COLOR_SECONDARY} fontSize='16px' align='justify'>
+                            <Grid item sm={8} xs={12}>
+                                <Typography color={FONT_COLOR_SECONDARY} fontSize='16px' align='justify' width={{ xs: 'auto', md: '70ch' }} margin='auto'>
                                     {data?.ourMission}
                                 </Typography>
                             </Grid>
                         </Grid>
                     </Container>
                 </Box>
-                <Container sx={{ p: 4 }} maxWidth='lg'>
-                    <Typography color={'#000'} fontSize='1.5rem' mb={2}>
+                <Container sx={{ p: 4, minHeight: '60vh' }} maxWidth='lg'>
+                    <Typography color={'#000'} fontSize='1.5rem' fontWeight={600} mb={2}>
                         The Start of a new Carrier Ecosystem:
                     </Typography>
                     <Typography color={FONT_COLOR_SECONDARY} align='justify' dangerouslySetInnerHTML={{ __html: data?.carrierEcoSystem }}>
